a8275ca8a81b52a1b75dd9de1be70ad9fb497c1d
Nico <oprescunico@gmail.com>
2024-04-14 02:01:34 +0100

    Added Visual Assets

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -13,6 +13,11 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int speed = 5;
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
 }
