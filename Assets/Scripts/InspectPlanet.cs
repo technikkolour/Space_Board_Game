ed236a6b0c407ca89fba5af18343acc57ea80b64
Nico <oprescunico@gmail.com>
2024-04-14 03:49:15 +0100

    Began Implementation of Solar System

--- Assets/Scripts/InspectPlanet.cs
+++ Assets/Scripts/InspectPlanet.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class InspectPlanet : MonoBehaviour
 {
+    string[] FunFacts = { "Fun Fact: Mercury is the smallest planet and zooms around the Sun super quick, in just 88 days!",
+        "Fun Fact: Venus is really, really hot, even hotter than your oven! On its surface you can find lava and even witness acid rain!",
+        "Fun Fact: Earth is special because it has air and water, and it's where all of us live!",
+        "Fun Fact: Mars looks red and has the biggest volcano and the biggest dust storms!",
+        "Fun Fact: Jupiter is the biggest planet and has a huge storm called the Great Red Spot.",
+        "Fun Fact: Saturn has shiny rings around it and lots of moons.",
+        "Fun Fact: Uranus spins on its side and is a pretty blue color.",
+        "Fun Fact: Far, far away: Neptune is the farthest planet from the Sun and has super strong winds."};
+
     // Start is called before the first frame update
     void Start()
     {
